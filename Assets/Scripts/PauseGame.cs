4785fc718c357c31fc41d27e7388418744b396fa
MadMonk <marcin.majewski.programista@gmail.com>
2018-09-15 17:59:48 +0200

    before adding shop menu

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour {
+
+   // Button button;
+	// Use this for initialization
+	void Start () {
+
+     //   button = GetComponent<Button>();
+    //    button.onClick.AddListener(Pause);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+}
