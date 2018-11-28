413b6b2e16a093e20f072df9878751dbddf62052
MadMonk <marcin.majewski.programista@gmail.com>
2018-11-28 19:20:48 +0100

    Joystick implemented
    -controls based on joystick pack
    -added buttons for quit

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -5,8 +5,18 @@ using UnityEngine.EventSystems;
 
 public class ShipMovement : MonoBehaviour {
 
+
     [SerializeField]
-    Vector2 MovementArea;
+    float MaxPositionX = 5f;
+
+    [SerializeField]
+    float MaxPositionY = 5f;
+
+   // [SerializeField]
+   // Vector2 MovementArea;
+
+    [SerializeField]
+    Joystick joystick;
 
     Camera Camera;
 
@@ -19,7 +29,7 @@ public class ShipMovement : MonoBehaviour {
     private void OnDrawGizmos()
     {
         //położenie + wielkość
-        Gizmos.DrawWireCube(Vector3.up*2.8f, MovementArea * 2f);
+    //    Gizmos.DrawWireCube(Vector3.up*2.8f, MovementArea * 2f);
     }
 
 
@@ -28,22 +38,50 @@ public class ShipMovement : MonoBehaviour {
         //odczytujem połozenie myszy i konwertujemy je na położenie w świecie gry 3d
 
 
-        if (!(Input.GetMouseButton(0))) return;
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.up * joystick.Vertical);
 
-      
+        if (moveVector != Vector3.zero)
+        {
 
-        if (!(EventSystem.current.IsPointerOverGameObject()))
-        { 
-        var targetPosition = (Vector2)Camera.ScreenToWorldPoint(Input.mousePosition);
+            transform.Translate(moveVector * Time.deltaTime * 4f, Space.World);
 
-        //ograniczamy pole manewru statku, żeby nie wyleciał za plansze
-        targetPosition.x = Mathf.Clamp(targetPosition.x, -MovementArea.x, MovementArea.x);
-        targetPosition.y = Mathf.Clamp(targetPosition.y, -MovementArea.y, MovementArea.y);
+            var positionX = Mathf.Clamp(transform.position.x, -MaxPositionX, MaxPositionX);
+           
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
+            var positionY = Mathf.Clamp(transform.position.y, -MaxPositionY, MaxPositionY);
+            //funkcja Clamp zwraca daną wartość z  określonego przedziału
+            //jeżeli ta wartość x zawiera się w przedziale to jest zwracana, w innym przypadku zwracana jest skrajna wartość
+
+            transform.position = new Vector3(positionX, positionY);
+
+           
+            // transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
+        }
+
+        /* 
+         *   if (moveVector != Vector3.zero)
+          { 
+              transform.rotation = Quaternion.LookRotation(Vector3.forward, moveVector);
+              transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
+          }
+         
+         * if (!(Input.GetMouseButton(0))) return;
+
+
+
+          if (!(EventSystem.current.IsPointerOverGameObject()))
+          { 
+          var targetPosition = (Vector2)Camera.ScreenToWorldPoint(Input.mousePosition);
+
+          //ograniczamy pole manewru statku, żeby nie wyleciał za plansze
+          targetPosition.x = Mathf.Clamp(targetPosition.x, -MovementArea.x, MovementArea.x);
+          targetPosition.y = Mathf.Clamp(targetPosition.y, -MovementArea.y, MovementArea.y);
+
+          transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
+          */
     }
-        
 
-    }
 
 }
+
+
